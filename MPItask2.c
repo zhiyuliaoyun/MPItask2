3ebb948672ee269cfa9de136ca2fb8f8e86b7f4b
zhiyuliaoyun <79763276+zhiyuliaoyun@users.noreply.github.com>
2021-03-27 23:22:04 +0300

    Update MPItask2.c

--- MPItask2.c
+++ MPItask2.c
@@ -3,15 +3,16 @@
 #include <mpi.h>
 #include <malloc.h>
 #include <math.h>
-float f(float x){
+#define min(x, y) ((x) > (y) ? (y) : (x))
+float f(float x) {
 	return x;
 }
-float Trap(float a, float b, float j){
+float Trap(float a, float b, float j) {
 	float h = (b - a) / j, T = 0;
 	for (float i = 1; i < j; i++)  T += f(a + i * h);
 	return h * (f(a) + 2 * T + f(b)) / 2;
 }
-int main(int argc, char *argv[]){
+main(int argc, char** argv) {
 	int numProcs, my_rank;
 	int i;
 	float k = 0;
@@ -23,22 +24,22 @@ int main(int argc, char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
 	if (my_rank == 0) {
-		for (int i = 1; i <= numProcs; i++) {
-			MPI_Send(&k, 1, MPI_FLOAT, i, MPI_ANY_TAG, MPI_COMM_WORLD);
+		for (i = 1; i < min(numProcs, 1024); i++) {
+			MPI_Send(&k, 1, MPI_FLOAT, i, 20, MPI_COMM_WORLD);
 			k = k + 1;
 		}
-		for (int i = 0; i < 1024; i++) {
+		for (i = 0; i < 1024; i++) {
 			MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			int process_rank = status.MPI_SOURCE;
 			MPI_Recv(&part_trap, 1, MPI_FLOAT, process_rank, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			total += part_trap;
 			if (k < 1023) {
 				k = k + 1;
-				MPI_Send(&k, 1, MPI_FLOAT, process_rank, MPI_ANY_TAG, MPI_COMM_WORLD);
+				MPI_Send(&k, 1, MPI_FLOAT, process_rank, 20, MPI_COMM_WORLD);
 			}
 			else {
 				float close = 1.0;
-				MPI_Send(&close, 1, MPI_FLOAT, process_rank, 0, MPI_COMM_WORLD);
+				MPI_Send(&close, 1, MPI_FLOAT, process_rank, 10, MPI_COMM_WORLD);
 			}
 		}
 		printf(" the integral of f(x)=x from 10 to 100 is %1.3f\t", total);
@@ -46,11 +47,16 @@ int main(int argc, char *argv[]){
 	else {
 		while (1) {
 			MPI_Recv(&k, 1, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			float l = (b - a) / n;
-			float part_a = a + k * l;
-			float part_b = a + (k + 1) * l;
-			part_trap = Trap(part_a, part_b, 10);
-			MPI_Send(&part_trap, 1, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD);
+			if (status.MPI_TAG != 10) {
+				float l = (b - a) / n;
+				float part_a = a + k * l;
+				float part_b = a + (k + 1) * l;
+				part_trap = Trap(part_a, part_b, 10);
+				MPI_Send(&part_trap, 1, MPI_FLOAT, 0, status.MPI_TAG, MPI_COMM_WORLD);
+			}
+			else {
+				break;
+			}
 		}
 	}
 	MPI_Finalize();
